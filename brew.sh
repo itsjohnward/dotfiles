c36fbcb9841e91cd4c2774985760df48cc86d993
John Ward <jsward2@gmail.com>
2015-06-13 22:36:37 -0400

    init

---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install atom
+brew cask install blender
+brew cask intall gimp
+brew cask install xquartz
+brew cask install inkscape
+brew cask install audacity
+brew cask install spectacle
+brew cask install caffeine
