875d1de84a658cf99f4053b66101e01d15b153d4
John Ward <john@itsjohnward.com>
2018-06-27 00:16:55 -0400

    Remove extra paren

--- windows/config.ps1
+++ windows/config.ps1
@@ -1,12 +1,12 @@
 ./windows/install-chocolatey # Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # install programs with chocolatey
-foreach ($line in (Invoke-webrequest -URI "https://raw.githubusercontent.com/itsjohnward/dotfiles/master/windows/choco-programs.txt").Content -split "\n")) {
+foreach ($line in (Invoke-webrequest -URI "https://raw.githubusercontent.com/itsjohnward/dotfiles/master/windows/choco-programs.txt").Content -split "\n") {
     choco install $line
 }
 
 # install vscode extensions
-foreach ($line in (Invoke-webrequest -URI "https://raw.githubusercontent.com/itsjohnward/dotfiles/master/vscode/extensions.txt").Content -split "\n")) {
+foreach ($line in (Invoke-webrequest -URI "https://raw.githubusercontent.com/itsjohnward/dotfiles/master/vscode/extensions.txt").Content -split "\n") {
     code --install-extension $line
 }
 # configure vscode
