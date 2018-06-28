b520a03138d210d7855914f16440da57f20a8d78
John Ward <john@itsjohnward.com>
2018-06-27 22:35:14 -0400

    Use copy instead of download and add file extension to choco-programs

--- windows/config.ps1
+++ windows/config.ps1
@@ -1,7 +1,7 @@
 ./windows/install-chocolatey # Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # install programs with chocolatey
-foreach ($line in Get-Content windows/choco-programs) {
+foreach ($line in Get-Content windows/choco-programs.txt) {
     choco install $line
 }
 
@@ -12,9 +12,9 @@ foreach ($line in Get-Content vscode/extensions.txt) {
     code --install-extension $line
 }
 # configure vscode
-(New-Object System.Net.WebClient).DownloadFile( 'https://raw.githubusercontent.com/itsjohnward/dotfiles/master/vscode/settings.json', "$env:APPDATA/Code/User/settings.json" )
+Copy-Item "vscode/settings.json" -Destination "$env:APPDATA/Code/User/settings.json"
 
 # configure terminus
-(New-Object System.Net.WebClient).DownloadFile( 'https://raw.githubusercontent.com/itsjohnward/dotfiles/master/windows/terminus/config.yaml', "$env:APPDATA/Terminus/config.yaml" )
+Copy-Item "windows/terminus/config.yaml" -Destination "$env:APPDATA/Terminus/config.yaml"
 
 # Enter bash and execute playbook with "-t windows"
