a9e82546b867e875a37535503680dcc4ad29b0cf
John Ward <john@itsjohnward.com>
2018-06-27 22:40:20 -0400

    Add test output

--- windows/config.ps1
+++ windows/config.ps1
@@ -11,8 +11,15 @@ $env:Path += ";C:\Program Files\Microsoft VS Code" # register the new "code" com
 foreach ($line in Get-Content vscode/extensions.txt) {
     code --install-extension $line
 }
+
+ls "$env:APPDATA/"
+ls "$env:APPDATA/Code/"
+ls "$env:APPDATA/Code/User/"
+
 # configure vscode
 Copy-Item "vscode/settings.json" -Destination "$env:APPDATA/Code/User/settings.json"
+
+ls "$env:APPDATA/Terminus/"
 
 # configure terminus
 Copy-Item "windows/terminus/config.yaml" -Destination "$env:APPDATA/Terminus/config.yaml"
