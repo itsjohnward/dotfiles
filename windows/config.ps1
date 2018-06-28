87ed4800124f3f6a305e9c1da73c4cb33ebb5be3
John Ward <john@itsjohnward.com>
2018-06-27 22:46:13 -0400

    make directories that dont exist yet

--- windows/config.ps1
+++ windows/config.ps1
@@ -12,14 +12,13 @@ foreach ($line in Get-Content vscode/extensions.txt) {
     code --install-extension $line
 }
 
-ls "$env:APPDATA/"
-ls "$env:APPDATA/Code/"
-ls "$env:APPDATA/Code/User/"
+mkdir "$env:APPDATA/Code/"
+mkdir "$env:APPDATA/Code/User/"
 
 # configure vscode
 Copy-Item "vscode/settings.json" -Destination "$env:APPDATA/Code/User/settings.json"
 
-ls "$env:APPDATA/Terminus/"
+mkdir "$env:APPDATA/Terminus/"
 
 # configure terminus
 Copy-Item "windows/terminus/config.yaml" -Destination "$env:APPDATA/Terminus/config.yaml"
