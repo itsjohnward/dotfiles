2e5ccd4aac16d957182004edb2eea22298ed5e82
John Ward <john@itsjohnward.com>
2018-07-05 21:52:08 -0400

    Split install python out into its own file and update bash refresh

---
+++ scripts/install-python.sh
@@ -0,0 +1,30 @@
+# install pyenv prerequsites
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev python
+
+# make sure pip and virtualenv are installed
+python -m pip install --user --upgrade pip
+python -m pip install --user virtualenv
+
+# install pyenv
+# curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+# install pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+
+exec "$BASH"
+
+# install pyenv plugins
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(PYENV_ROOT)/plugins/pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git $(PYENV_ROOT)/plugins/pyenv-virtualenvwrapper
+echo 'PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"' >> ~/.bashrc
+echo 'pyenv virtualenvwrapper_lazy' >> ~/.bashrc
+git clone git://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
+
+exec "$BASH"
+
+pyenv update
