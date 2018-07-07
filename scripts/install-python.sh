c151c82def72bb1d9cb6aba7ffd49797c621da53
John Ward <john@itsjohnward.com>
2018-07-06 22:30:37 -0400

    Take bash refreshing out of install python script and update appveyor command

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -16,15 +16,9 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 
-exec "$BASH"
-
 # install pyenv plugins
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git $(pyenv root)/plugins/pyenv-virtualenvwrapper
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(PYENV_ROOT)/plugins/pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git $(PYENV_ROOT)/plugins/pyenv-virtualenvwrapper
 echo 'PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"' >> ~/.bashrc
 echo 'pyenv virtualenvwrapper_lazy' >> ~/.bashrc
 git clone git://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
-
-exec "$BASH"
-
-pyenv update
