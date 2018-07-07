5227400e52acc178b128d139240ab70d59624fce
John Ward <john@itsjohnward.com>
2018-07-07 01:20:19 -0400

    Add missing quote

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -15,7 +15,7 @@ locate -br '^virtualenvwrapper.sh$'
 echo locate -br '^virtualenvwrapper.sh$'
 echo '---'
 
-echo 'source "$HOME/.local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+echo 'source "$HOME/.local/bin/virtualenvwrapper.sh"' >> ~/.bashrc
 
 # install pyenv
 # curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
