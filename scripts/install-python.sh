37642c4dfaac343ea1f74a8cb1ebf5e1a6b95cd1
John Ward <john@itsjohnward.com>
2018-07-07 01:56:47 -0400

    Remove lazy

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -10,7 +10,7 @@ python -m pip install --user virtualenv virtualenvwrapper
 echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
 echo 'export PROJECT_HOME=$HOME' >> ~/.bashrc
 
-echo 'source "$HOME/.local/bin/virtualenvwrapper_lazy.sh"' >> ~/.bashrc
+echo 'source "$HOME/.local/bin/virtualenvwrapper.sh"' >> ~/.bashrc
 
 # install pyenv
 # curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
@@ -25,5 +25,5 @@ echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nf
 git clone https://github.com/pyenv/pyenv-virtualenv.git "$HOME/.pyenv/plugins/pyenv-virtualenv"
 git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git "$HOME/.pyenv/plugins/pyenv-virtualenvwrapper"
 echo 'PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"' >> ~/.bashrc
-echo 'pyenv virtualenvwrapper_lazy' >> ~/.bashrc
+echo 'pyenv virtualenvwrapper' >> ~/.bashrc
 git clone git://github.com/pyenv/pyenv-update.git "$HOME/.pyenv/plugins/pyenv-update"
