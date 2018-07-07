cd1cb0ec5c3566f92d66b59612b88557aa87b854
John Ward <john@itsjohnward.com>
2018-07-07 02:15:34 -0400

    Add extra bashrc source

--- ubuntu.bootstrap.sh
+++ ubuntu.bootstrap.sh
@@ -5,6 +5,8 @@ cd dotfiles
 
 # activate the project's virtual environment and install dependencies
 pyenv install --skip-existing
+source ~/.bashrc
+pyenv virtualenvwrapper
 mkvirtualenv -r requirements.txt py-env
 
 ansible-playbook playbooks/main.yml --connection=local -t linux,ubuntu
