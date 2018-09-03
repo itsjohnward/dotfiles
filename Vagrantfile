96de7846e5077f54b4fb6a30b988738fd9be08ac
John Ward <jsward2@gmail.com>
2018-09-02 20:02:00 -0400

    Add Vagrantfile - close #2 - and update tmux and python

---
+++ Vagrantfile
@@ -0,0 +1,4 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "bento/ubuntu-18.04"
+  config.vm.provision "shell", path: "https://raw.githubusercontent.com/itsjohnward/dotfiles/master/ubuntu.sh"
+end
--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# upgrade system
-# execute install scripts
-# stow configs
+echo 'Executing ubuntu.sh...'
+
+sudo apt update && sudo apt upgrade && sudo apt autoremove
+
+git clone --recurse-submodules https://github.com/itsjohnward/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+
+./install/ubuntu.python.sh
